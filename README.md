d27ea300dcb24eac24bfa7173043ba99e8f5224d
Georg <georg.schwan@vtxnet.ch>
2020-02-25 17:45:53 +0100

    changed how settings work

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Installation
+0. make sure at least python 3.6 is installed
 1. cd in this directory
 2. run `pip install -r requirements.txt`
 3. run `python setup.py`
 4. make sure the values in `settings.config` are correct
-5. run `python main.py`
+5. run `python main.py` or execute the `run.bat` file
 
-## Execute from a .bat file
-1. Make a .bat file with the content "python path\to\main.py"
-2. You can write on the second line "pause", so that the prompt doesn't close automatic
+    Note: You can use the `run.bat` file from anywhere
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import shutil
 def setup():
     base_path = os.path.dirname(__file__)
     settings_path = os.path.join(base_path, "settings.config")
+    bat_file_path = os.path.join(base_path, "run.bat")
     template_path = os.path.join(base_path, "settings\\settings.config.template")
 
     settings = {}
@@ -24,6 +25,10 @@ def setup():
     with open(settings_path, "w+") as f:
         for key, value in settings.items():
             f.write(f"{key}={value}\n")
+
+    with open(bat_file_path, "w+") as f:
+        f.write(f"python {os.path.join(base_path, 'main.py')}\n")
+        f.write("pause")
 
 
 if __name__ == "__main__":
