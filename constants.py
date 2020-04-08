141e2257a99d3a061e429d236fa04348c3df141b
GeorgOhneH <georg.schwan@vtxnet.ch>
2020-04-08 23:23:50 +0200

    test

--- constants.py
+++ constants.py
@@ -5,5 +5,5 @@ try:
     BeautifulSoup("", "lxml")
     BEAUTIFUL_SOUP_PARSER = "lxml"
 except bs4.FeatureNotFound:
-    print("It appears that lxml is not installed. Falling back to html.parser")
+    print("It appears that 'lxml' is not installed. Falling back to 'html.parser'")
     BEAUTIFUL_SOUP_PARSER = "html.parser"
