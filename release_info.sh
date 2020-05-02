3f892e560059621b12c9ff44e74193b7281d4360
Roland Schwan <roland.schwan@mikrounix.com>
2020-05-02 17:18:22 +0200

    fix releases

--- release_info.sh
+++ release_info.sh
@@ -3,5 +3,5 @@
 LATEST_RELEASE="$(curl -s https://api.github.com/repos/GeorgOhneH/ethz-document-fetcher/releases/latest | jq -r '.tag_name')"
 CURRENT_VERSION="$(cat version.txt)"
 
-echo "::set-env name=latest_release::$LATEST_RELEASE"
-echo "::set-env name=current_version::$CURRENT_VERSION"
+echo "::set-output name=latest_release::$LATEST_RELEASE"
+echo "::set-output name=current_version::$CURRENT_VERSION"
