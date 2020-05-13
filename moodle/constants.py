e389f848410cf237d83e20b6d43a70b31c344501
GeorgOhneH <georg.schwan@vtxnet.ch>
2020-05-13 17:33:24 +0200

    added checksum by moodle

--- moodle/constants.py
+++ moodle/constants.py
@@ -8,3 +8,12 @@ MTYPE_FILE = "resource"
 MTYPE_EXTERNAL_LINK = "url"
 MTYPE_DIRECTORY = "folder"
 
+UPDATE_REQUEST_PAYLOAD = [{
+    "index": 0,
+    "methodname": "core_course_get_updates_since",
+    "args": {
+        "courseid": 12228,
+        "since": 0,
+    }
+}]
+AJAX_SERVICE_URL = "https://moodle-app2.let.ethz.ch/lib/ajax/service.php"
--- moodle/parser.py
+++ moodle/parser.py
@@ -9,56 +9,66 @@ import polybox
 from core.constants import BEAUTIFUL_SOUP_PARSER
 from core.utils import *
 from settings import settings
-from .constants import MTYPE_EXTERNAL_LINK, MTYPE_DIRECTORY, MTYPE_FILE, PDF_IMAGE
+from .constants import *
 
 logger = logging.getLogger(__name__)
 
 
 async def parse_main_page(session, queue, html, base_path, moodle_id, use_external_links):
+    sesskey = re.search(b"""sesskey":"([^"]+)""", html)[1].decode("utf-8")
+    async with session.post(AJAX_SERVICE_URL, json=get_update_payload(moodle_id),
+                            params={"sesskey": sesskey}) as response:
+        update_json = await response.json()
+
+    last_updated_dict = parse_update_json(update_json)
+
     only_sections = SoupStrainer("li", id=re.compile("section-([0-9]+)"))
     soup = BeautifulSoup(html, BEAUTIFUL_SOUP_PARSER, parse_only=only_sections)
 
     sections = soup.find_all("li", id=re.compile("section-([0-9]+)"), recursive=False)
 
-    coroutines = [parse_sections(session, queue, section, base_path, moodle_id, use_external_links)
+    coroutines = [parse_sections(session, queue, section, base_path, moodle_id, use_external_links, last_updated_dict)
                   for section in sections]
     await asyncio.gather(*coroutines)
 
 
-async def parse_sections(session, queue, section, base_path, moodle_id, use_external_links):
+async def parse_sections(session, queue, section, base_path, moodle_id, use_external_links, last_updated_dict):
     section_name = str(section["aria-label"])
     base_path = safe_path_join(base_path, section_name)
 
-    instances = section.find_all("div", class_="activityinstance")
-
+    modules = section.find_all("li", id=re.compile("module-[0-9]+"))
     tasks = []
-    for instance in reversed(instances):
-        try:
-            instance.a.span
-        except AttributeError:
-            continue
-
-        mtype = instance.parent.parent.parent.parent["class"][1]
+    for module in modules:
+        mtype = module["class"][1]
+        module_id = int(re.search("module-([0-9]+)", module["id"])[1])
 
         if mtype == MTYPE_FILE:
-            file_name = str(instance.a.span.contents[0])
+            instance = module.find("div", class_="activityinstance")
+            try:
+                file_name = str(instance.a.span.contents[0])
+            except AttributeError:
+                continue
+            last_updated = last_updated_dict[module_id]
+
             with_extension = False
             if instance.a.img["src"] == PDF_IMAGE:
                 file_name += ".pdf"
                 with_extension = True
 
             url = instance.a["href"] + "&redirect=1"
-            await queue.put({"path": safe_path_join(base_path, file_name),
-                             "url": url, "with_extension": with_extension})
+            await queue.put({"path": safe_path_join(base_path, file_name), "url": url,
+                             "with_extension": with_extension, "checksum": last_updated})
 
         elif mtype == MTYPE_DIRECTORY:
-            coroutine = parse_folder(session, queue, instance, base_path)
+            last_updated = last_updated_dict[module_id]
+            coroutine = parse_folder(session, queue, module, base_path, last_updated)
             tasks.append(asyncio.create_task(coroutine))
 
         elif mtype == MTYPE_EXTERNAL_LINK:
             if not use_external_links:
                 continue
 
+            instance = module.find("div", class_="activityinstance")
             url = instance.a["href"] + "&redirect=1"
             name = str(instance.a.span.contents[0])
 
@@ -78,10 +88,16 @@ async def parse_sections(session, queue, section, base_path, moodle_id, use_exte
             if coroutine is not None:
                 tasks.append(asyncio.create_task(exception_handler(coroutine, moodle_id, driver_url)))
 
-    await asyncio.gather(parse_sub_folders(queue, soup=section, folder_path=base_path), *tasks)
+    await asyncio.gather(*tasks)
 
 
-async def parse_folder(session, queue, instance, base_path):
+async def parse_folder(session, queue, module, base_path, last_updated):
+    folder_tree = module.find("div", id=re.compile("folder_tree[0-9]+"), class_="filemanager")
+    if folder_tree is not None:
+        await parse_folder_tree(queue, folder_tree.ul, base_path, last_updated)
+        return
+
+    instance = module.find("div", class_="activityinstance")
     folder_name = str(instance.a.span.contents[0])
     href = instance.a["href"]
 
@@ -91,16 +107,16 @@ async def parse_folder(session, queue, instance, base_path):
     only_file_tree = SoupStrainer("div", id=re.compile("folder_tree[0-9]+"), class_="filemanager")
     folder_soup = BeautifulSoup(text, BEAUTIFUL_SOUP_PARSER, parse_only=only_file_tree)
     folder_path = safe_path_join(base_path, folder_name)
-    await parse_sub_folders(queue, soup=folder_soup, folder_path=folder_path)
+    await parse_sub_folders(queue, soup=folder_soup, folder_path=folder_path, last_updated=last_updated)
 
 
-async def parse_sub_folders(queue, soup, folder_path):
+async def parse_sub_folders(queue, soup, folder_path, last_updated):
     folder_trees = soup.find_all("div", id=re.compile("folder_tree[0-9]+"), class_="filemanager")
     for folder_tree in folder_trees:
-        await parse_folder_tree(queue, folder_tree.ul, folder_path)
+        await parse_folder_tree(queue, folder_tree.ul, folder_path, last_updated)
 
 
-async def parse_folder_tree(queue, soup, folder_path):
+async def parse_folder_tree(queue, soup, folder_path, last_updated):
     children = soup.find_all("li", recursive=False)
     for child in children:
         if child.find("div", recursive=False) is not None:
@@ -109,12 +125,12 @@ async def parse_folder_tree(queue, soup, folder_path):
             sub_folder_path = folder_path
 
         if child.find("ul", recursive=False) is not None:
-            await parse_folder_tree(queue, child.ul, sub_folder_path)
+            await parse_folder_tree(queue, child.ul, sub_folder_path, last_updated)
 
         if child.find("span", recursive=False) is not None:
             url = child.span.a["href"]
             name = child.span.a.find("span", recursive=False, class_="fp-filename").get_text(strip=True)
-            await queue.put({"path": safe_path_join(sub_folder_path, name), "url": url})
+            await queue.put({"path": safe_path_join(sub_folder_path, name), "url": url, "checksum": last_updated})
 
 
 def poly_box_wrapper(func, moodle_id):
@@ -127,6 +143,7 @@ def poly_box_wrapper(func, moodle_id):
             else:
                 poly_id = args[2]
             logger.warning(f"Couldn't access polybox with id: {poly_id} from moodle: {moodle_id}")
+
     return wrapper
 
 
@@ -140,3 +157,38 @@ async def exception_handler(coroutine, moodle_id, url):
             traceback.print_exc()
         logger.error(f"Got an unexpected error from moodle: {moodle_id} "
                      f"while trying to access {url}, Error: {type(e).__name__}: {e}")
+
+
+def get_update_payload(courseid, since=0):
+    return [{
+        "index": 0,
+        "methodname": "core_course_get_updates_since",
+        "args": {
+            "courseid": courseid,
+            "since": since,
+        }
+    }]
+
+
+def parse_update_json(update_json):
+    if update_json[0]["error"]:
+        raise ValueError("update json has an error")
+
+    result = {}
+    for instance in update_json[0]["data"]["instances"]:
+        if instance["contextlevel"] != "module":
+            continue
+
+        instance_id = instance["id"]
+        last_update = None
+        for update in instance["updates"]:
+            if update["name"] == "configuration":
+                last_update = update["timeupdated"]
+                break
+
+        if last_update is None:
+            raise ValueError(f"Did not found a timeupdated field in {instance['updates']}")
+
+        result[instance_id] = last_update
+
+    return result
