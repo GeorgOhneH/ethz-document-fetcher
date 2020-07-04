ffec6c681b9bfded9c96a2adcebc812a77073401
GeorgOhneH <georg.schwan@vtxnet.ch>
2020-07-04 15:56:03 +0200

    updated checkbox?

--- gui/template_view/view_tree.py
+++ gui/template_view/view_tree.py
@@ -125,6 +125,7 @@ class TemplateViewTree(QTreeWidget):
     def set_check_state_to_all(self, state):
         for widget in self.widgets.values():
             widget.setCheckState(widget.COLUMN_NAME, state)
+        self.viewport().update()
 
     def get_checked(self):
         return [widget for widget in self.widgets.values() if widget.checkState(widget.COLUMN_NAME) != Qt.Unchecked]
