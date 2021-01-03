26aea8e4be677713d83bd0e78b314ad684fbfbf8
GeorgOhneH <georg.schwan@vtxnet.ch>
2021-01-04 00:44:37 +0100

    fix

--- core/client_config.py
+++ core/client_config.py
@@ -6,4 +6,4 @@ class ClientConfig(object):
     COMPANY_NAME = 'ethz-document-fetcher'
     HTTP_TIMEOUT = 30
     MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = [f'https://ethz-document-fetcher-release.s3.eu-central-1.amazonaws.com/{sys.platform}']
+    UPDATE_URLS = [f'https://ethz-document-fetcher-{sys.platform}.s3.eu-central-1.amazonaws.com']
