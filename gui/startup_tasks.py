593ff673c2eed8f60a9a45ffb50a6a2cc05ccb73
Georg <georg.schwan@vtxnet.ch>
2023-09-18 13:05:23 +0200

    fix

--- gui/startup_tasks.py
+++ gui/startup_tasks.py
@@ -2,12 +2,10 @@ import logging
 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from pyupdater.client import Client
 
 import core.utils
 import gui
-from core.client_config import ClientConfig
-from core.constants import VERSION, PYU_VERSION, IS_FROZEN
+from core.constants import VERSION, IS_FROZEN
 
 logger = logging.getLogger(__name__)
 
@@ -21,49 +19,13 @@ def run_startup_tasks(download_settings):
     behavior_settings = gui.Application.instance().behavior_settings
 
     if behavior_settings.check_for_updates and IS_FROZEN:
-        mutex = QMutex()
-        cond = QWaitCondition()
-        check_for_update = Update(mutex=mutex, cond=cond)
+        check_for_update = Update()
         QThreadPool.globalInstance().start(check_for_update)
-
-        check_for_update.signals.ask_for_permission.connect(
-            lambda latest_version: ask_update_pop_up(latest_version, check_for_update))
-
-
-def ask_update_pop_up(latest_version, check_for_update):
-    try:
-        msg_box = QMessageBox()
-        msg_box.setIcon(QMessageBox.Question)
-        msg_box.setWindowTitle("A new Version is available")
-        msg_box.setText(f"Version {latest_version[1:]} is available. (Current: {VERSION})\n"
-                        f"Do you want to Update?\n"
-                        f"(This will close and restart the app. It may take some time)")
-        msg_box.addButton(QMessageBox.Cancel)
-        install_button = msg_box.addButton("Update", QMessageBox.AcceptRole)
-
-        msg_box.setDefaultButton(install_button)
-
-        msg_box.exec()
-
-        if msg_box.clickedButton() != install_button:
-            return
-
-        check_for_update.allowed_download = True
-    finally:
-        check_for_update.cond.wakeAll()
-
-
-class Signals(QObject):
-    ask_for_permission = pyqtSignal(str)
 
 
 class Update(QRunnable):
-    signals = Signals()
-
-    def __init__(self, mutex, cond):
+    def __init__(self, ):
         super().__init__()
-        self.mutex = mutex
-        self.cond = cond
         self.allowed_download = False
 
     def run(self):
@@ -76,31 +38,17 @@ class Update(QRunnable):
         if latest_version == VERSION:
             return
 
-        client = Client(ClientConfig())
-        client.refresh()
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Question)
+        msg_box.setWindowTitle("A new Version is available")
+        msg_box.setText(f"Version {latest_version[1:]} is available. (Current: {VERSION})\n"
+                        f"Go to https://github.com/GeorgOhneH/ethz-document-fetcher/releases/latest?")
+        msg_box.addButton(QMessageBox.Cancel)
+        install_button = msg_box.addButton("Ok", QMessageBox.AcceptRole)
 
-        app_update = client.update_check(ClientConfig.APP_NAME, PYU_VERSION)
+        msg_box.setDefaultButton(install_button)
 
-        if app_update is None:
-            return
-
-        app_update.download()
-
-        self.signals.ask_for_permission.emit(latest_version)
-
-        self.mutex.lock()
-        try:
-            self.cond.wait(self.mutex)
-        finally:
-            self.mutex.unlock()
-
-        if not self.allowed_download:
-            logger.debug("Update declined")
-            return
-
-        if app_update.is_downloaded():
-            logger.debug("Update: Extract and Restart")
-            app_update.extract_restart()
+        msg_box.exec()
 
 
 class BackgroundTasks(QRunnable):
